8b427dc23c9949eed3452abc28e738bdb1d83e6f
steve <529543738@qq.com>
2015-11-20 09:06:35 +0800

    添加各种测试

--- src/main/java/com/example/TemplatesApplication.java
+++ src/main/java/com/example/TemplatesApplication.java
@@ -1,9 +1,19 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.AuthenticationManagerConfiguration;
+import org.springframework.boot.autoconfigure.security.BootGlobalAuthenticationConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.servlet.configuration.WebMvcSecurityConfiguration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration(exclude={SecurityAutoConfiguration.class,WebMvcSecurityConfiguration.class,WebSecurityConfiguration.class,AuthenticationConfiguration.class,AuthenticationManagerConfiguration.class,BootGlobalAuthenticationConfiguration.class})
+@ComponentScan
 public class TemplatesApplication {
 
     public static void main(String[] args) {
